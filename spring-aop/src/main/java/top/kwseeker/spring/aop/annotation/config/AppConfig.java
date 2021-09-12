86d91c28c3bc8c3735cdfb7491267f727399a786
kwseeker <xiaohuileee@gmail.com>
2021-09-13 00:24:50 +0800

    清除无用模块

--- spring-aop/src/main/java/top/kwseeker/spring/aop/annotation/config/AppConfig.java
+++
@@ -1,7 +0,0 @@
-package top.kwseeker.spring.aop.annotation.config;
-
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@EnableAspectJAutoProxy
-public class AppConfig {
-}
--- spring-aop/src/main/java/top/kwseeker/spring/aop/annotation/config/MyAspect.java
+++
@@ -1,9 +0,0 @@
-package top.kwseeker.spring.aop.annotation.config;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class MyAspect {
-
-    //定义切入点
-}
